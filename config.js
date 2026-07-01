c8767589f39305609eeca2e4a5f3a7eaca8c69e7
callamrik-ctrl <callamrik@gmail.com>
2026-07-01 17:18:33 -0400

    Remove commented URL from config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.FATEH_TIME_CONFIG = {
-  // Paste the deployed Google Apps Script Web App URL here.
-  // Example: https://script.google.com/macros/s/AKfycbx.../exec
-  scriptUrl: "https://script.google.com/macros/s/AKfycbyzQvufVp_j5I26De7iGxwf5YNNMpLOcKin_Rn4mCUbWGFhxfEBnm8rNDLzxvYsk2mE/exec",
+  scriptUrl: "https://script.google.com/macros/s/AKfycbyzQvufVp_j5I26De7iGxwf5YNNMpLOcKin_Rn4mCUbWGFhxfEBnm8rNDLzxvYsk2mE/exec"
 };
